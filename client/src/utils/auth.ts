efb9b8c27c9311a1222e600ef531ec71f2fbd74e
Kayla E <kengelstad16@gmail.com>
2025-02-13 00:08:55 -0600

    changed logout redirect

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -44,7 +44,7 @@ class AuthService {
     // TODO: remove the token from localStorage
     localStorage.removeItem('id_token');
     // TODO: redirect to the login page
-    window.location.assign('/');
+    window.location.assign('/login');
   }
 }
 
